fcb7b39fa315ceccc985d2e7e6a62a46e25b8f31
rimsan <mohamed.rimsan@viewqwest.com>
2019-03-07 16:32:19 +0800

    identify button has changed

--- src/components/buttons/VQHubIdentifyButton.js
+++ src/components/buttons/VQHubIdentifyButton.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import history from '../../history'
 
 class VQHubIdentifyButton extends React.Component {
-    identifyHandler = () => { alert(this.props.id) }
-    render() { return <button onClick={this.identifyHandler}>Identify </button> }
+    identifyHandler = () => { history.pushState('/hub/' + this.props.id); }
+    render() { return <button onClick={this.identifyHandler}>Identify</button> }
 }
 
 export default VQHubIdentifyButton
