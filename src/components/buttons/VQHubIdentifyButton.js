93d1d48cdac4212808b32dca89c02b842bba6767
rimsan <mohamed.rimsan@viewqwest.com>
2019-03-07 16:27:26 +0800

    view space added

--- src/components/buttons/VQHubIdentifyButton.js
+++ src/components/buttons/VQHubIdentifyButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class VQHubIdentifyButton extends React.Component {
     identifyHandler = () => { alert(this.props.id) }
-    render() { return <button onClick={this.identifyHandler}>Identify</button> }
+    render() { return <button onClick={this.identifyHandler}>Identify </button> }
 }
 
 export default VQHubIdentifyButton
